cfe8c841a06e5d3f9e848b91103b3aa3bb430ab4
Mohamedjcali <101645646+Mohamedjcali@users.noreply.github.com>
2023-12-26 14:24:35 +0300

    Update

--- kaafi/kaafi-backend/user/sendotp.go
+++ kaafi/kaafi-backend/user/sendotp.go
@@ -7,9 +7,9 @@ import (
 	openapi "github.com/twilio/twilio-go/rest/verify/v2"
 )
 
-var TWILIO_ACCOUNT_SID string = "AC0db6ef3790d9106217135913857f82bc"
-var TWILIO_AUTH_TOKEN string = "5d5724cb76150062eab6e1f26524c619"
-var VERIFY_SERVICE_SID string = "VAd23ee4381a65d7929a175a199de806f4"
+var TWILIO_ACCOUNT_SID string = "<enter your twilio account>"
+var TWILIO_AUTH_TOKEN string = "<your twilio aouthtoken>"
+var VERIFY_SERVICE_SID string = "<your serviece sid>"
 var client *twilio.RestClient = twilio.NewRestClientWithParams(twilio.ClientParams{
 	Username: TWILIO_ACCOUNT_SID,
 	Password: TWILIO_AUTH_TOKEN,
